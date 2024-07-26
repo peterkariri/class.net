2d4d812de248ef415b483757ab8ad2a136f1d9be
peterkariri <peternjugunaw25@gmail.com>
2024-07-26 09:41:41 +0300

    positions

---
+++ css/indexx.js
@@ -0,0 +1,10 @@
+getElementById: Returns an element with a specified ID.
+getElementsByTagName: Returns a collection of elements with a specified tag name.
+getElementsByClassName: Returns a collection of elements with a specified class name.
+querySelector: Returns the first element that matches a specified CSS selector.
+querySelectorAll: Returns a collection of elements that match a specified CSS selector.
+createElement: Creates a new element.
+appendChild: Appends a child element to a parent element.
+removeChild: Removes a child element from a parent element.
+innerHTML: Gets or sets the HTML content of an element.
+textContent: Gets or sets the text content of an element.
